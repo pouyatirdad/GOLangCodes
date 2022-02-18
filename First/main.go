3645d06d7842a97fe9d23fa110a081ccdc85a3d7
pouyatirdad <pouyatirdad70@gmail.com>
2022-02-18 19:18:21 +0330

    add hello world

---
+++ First/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	// first code , hello world
+	fmt.Println("\n hello world from pouya in Go")
+
+	// hello world with variable
+	var Deutsch_Hello string
+	Deutsch_Hello = "Hallo Welt"
+
+	fmt.Printf("\n Hello World in German : %s \n", Deutsch_Hello)
+
+	//diffrent way
+	Deutsch_Hi := "Hallo Welt"
+	fmt.Printf("\n Hello World in German : %s \n \n", Deutsch_Hi)
+}
